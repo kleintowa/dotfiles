0d95d25492d209c4c65dd0e3a72ac870d052aefa
Mareike Heemann <mahe@akra.de>
2018-10-07 23:15:25 +0200

    Set up setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install bundle (Brewfile with all the stuff)
+brew bundle
